c3a307a37e8aa3ccae0cfe322b476845fb98170c
Matthew Gleich <email@mattglei.ch>
2021-01-01 17:19:36 -0500

    ✨ Add docker install script

---
+++ install-docker.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Installing docker onto a RPI
+
+# Install Docker:
+curl -sSL https://get.docker.com | sh
+
+# Giving Docker Sudo Privileges:
+sudo groupadd docker
+sudo gpasswd -a $USER docker
+
+# Installing docker compose
+
+sudo apt install docker-compose -y
+
+# Rebooting the pi
+echo "The pi will reboot in 5 seconds"
+sleep 5
+sudo reboot
+
